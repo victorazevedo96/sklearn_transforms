4c9baa4ae53ba697143fe40406d66669d124a342
victorazevedo96 <70180223+victorazevedo96@users.noreply.github.com>
2020-08-28 22:03:55 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -2,15 +2,35 @@ from sklearn.base import BaseEstimator, TransformerMixin
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
-class DropColumns(BaseEstimator, TransformerMixin):
-    def __init__(self, columns):
-        self.columns = columns
+class Faz_Tudo(BaseEstimator, TransformerMixin):
+    def __init__(self, colunas):
+        self.columns = colunas
 
     def fit(self, X, y=None):
         return self
 
     def transform(self, X):
-        # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
-        # Retornamos um novo dataframe sem as colunas indesejadas
-        return data.drop(labels=self.columns, axis='columns')
+        data = data.drop(columns=['INGLES'],axis=1)
+        ingles = pd.DataFrame(data=X['INGLES'])
+        data =  data.drop(labels = self.colunas,axis=1)
+
+        si = SimpleImputer(
+        missing_values=np.nan,  # os valores faltantes são do tipo ``np.nan`` (padrão Pandas)
+        strategy='mean',  # a estratégia escolhida é a alteração do valor faltante por uma constante
+        verbose=0,
+        copy=True
+    )
+        si.fit(X=data)
+        data_return = pd.DataFrame.from_records(data=si.transform( X=data),columns=data.columns)
+
+        si_ingles = SimpleImputer(
+        missing_values=np.nan,  # os valores faltantes são do tipo ``np.nan`` (padrão Pandas)
+        strategy='most_frequent',  # a estratégia escolhida é a alteração do valor faltante por uma constante
+        verbose=0,
+        copy=True
+        )
+        si_ingles.fit(X=ingles)
+        ingles = pd.DataFrame.from_records(data=si_ingles.transform(X=ingles),columns=['INGLES'])
+
+        data_return['INGLES'] = ingles
